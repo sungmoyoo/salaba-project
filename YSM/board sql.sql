fb728f9fee184fdc730072192a3a1151ea6906b1
sungmoyoo <samyoo644@gmail.com>
2024-03-25 21:00:50 +0900

    .

--- YSM/board sql.sql
+++ YSM/board sql.sql
@@ -1,10 +1,10 @@
 -- 커뮤니티 홈 화면 조회
 select
+  b.board_no,
   b.title,
   b.created_date,
   count(bl.member_no) like_count,
   m.name,
-  GROUP_CONCAT(distinct bf.uuid_file_name) uuid_file_name,
   h.head_content
 from
   board b
@@ -13,19 +13,41 @@ from
   left join board_like bl on b.board_no=bl.board_no
   left join board_file bf on b.board_no=bf.board_no
 group by
-  b.title,
-  b.created_date,
-  h.head_content,
-  m.name
+  b.board_no,
+  like_count
 order by
-  b.created_date desc;
+  like_count desc;
 
 -- 정보공유/자유 게시판 리스트 조회
 -- 정렬은 기본 최신순, 추천순, 조회순은 order by만 바꾸면 됨
 select 
+  b.board_no,
   b.title,
   b.view_count,
   b.created_date,
+  h.head_content,
+  m.name,
+  count(distinct bl.member_no) like_count,
+  count(distinct c.comment_no) + count(distinct r.reply_no) as comment_reply_count
+from 
+  board b 
+  inner join head h on b.head_no=h.head_no
+  inner join member m on b.member_no=m.member_no
+  inner join board_like bl on b.board_no=bl.board_no
+  left join comment c on b.board_no = c.board_no
+  left join reply r on c.comment_no = r.comment_no
+where
+  b.board_category_no=1 
+group by
+  b.board_no;
+
+
+-- 후기 게시판 리스트 조회
+select 
+  b.title,
+  b.view_count,
+  b.created_date,
+  bf.uuid_file_name uuid_file_name,
   h.head_content,
   m.name,
   count(bl.member_no) like_count
@@ -34,40 +56,12 @@ from
   inner join head h on b.head_no=h.head_no
   inner join member m on b.member_no=m.member_no
   left join board_like bl on b.board_no=bl.board_no
-where
-  b.board_category_no=1
-group by
-  b.title,
-  b.view_count,
-  b.created_date,
-  h.head_content,
-  m.name
-order by
-  b.created_date desc;
-
--- 후기 게시판 리스트 조회
-select 
-  b.title,
-  b.view_count,
-  b.created_date,
-  group_concat(bf.uuid_file_name) uuid_file_name,
-  h.head_content,
-  m.name,
-  count(bl.member_no) as like_count
-from 
-  board b 
-  inner join head h on b.head_no=h.head_no
-  inner join member m on b.member_no=m.member_no
-  left join board_like bl on b.board_no=bl.board_no
   left join board_file bf on b.board_no=bf.board_no
 where
-  b.board_category_no=2
+  b.board_category_no=3
 group by
-  b.title,
-  b.view_count,
-  b.created_date,
-  h.head_content,
-  m.name
+  b.board_no,
+  bf.uuid_file_name
 order by
   b.created_date desc;
 
@@ -96,11 +90,11 @@ from
   board b 
   inner join head h on b.head_no = h.head_no
   inner join member m on b.member_no = m.member_no
-  LEFT JOIN board_file bf on b.board_no = bf.board_no
-  LEFT JOIN comment c on b.board_no = c.board_no
-  LEFT JOIN reply r on c.comment_no = r.comment_no
-  LEFT JOIN member mc on c.member_no = mc.member_no
-  LEFT JOIN member mr on r.member_no = mr.member_no
+  left join board_file bf on b.board_no = bf.board_no
+  left join comment c on b.board_no = c.board_no
+  left join reply r on c.comment_no = r.comment_no
+  left join member mc on c.member_no = mc.member_no
+  left join member mr on r.member_no = mr.member_no
 where
   b.board_no = 1
 
@@ -108,36 +102,36 @@ where
 --뷰 조회 중복 합친 버전
 --사진, 댓글, 답글, 댓글작성자, 답글작성자를 구분자(,)로 하나의 컬럼으로 묶어 조회하는 방식
 --이 외 다른 해결 방법으로 생각나는 건... 각 테이블마다 값을 가져오는 방법인데 비효율적이지 않을까..
-select 
-  b.title,
-  b.view_count,
-  b.created_date,
-  GROUP_CONCAT(distinct bf.uuid_file_name) uuid_file_name,
-  h.head_content,
-  m.name,
-  (select 
-    COUNT(*) 
-  from 
-    board_like bl 
-  where 
-    bl.board_no = b.board_no) like_count,
-  GROUP_CONCAT(distinct c.content) comment_content,
-  GROUP_CONCAT(distinct c.created_date) comment_date,
-  GROUP_CONCAT(distinct r.content) reply_comment,
-  GROUP_CONCAT(distinct r.created_date) reply_date,
-  GROUP_CONCAT(distinct mc.name) comment_writer,
-  GROUP_CONCAT(distinct mr.name) reply_writer
-from 
-  board b 
-  inner join head h on b.head_no = h.head_no
-  inner join member m on b.member_no = m.member_no
-  LEFT JOIN board_file bf on b.board_no = bf.board_no
-  LEFT JOIN comment c on b.board_no = c.board_no
-  LEFT JOIN reply r on c.comment_no = r.comment_no
-  LEFT JOIN member mc on c.member_no = mc.member_no
-  LEFT JOIN member mr on r.member_no = mr.member_no
-where
-  b.board_no = 1;
+-- select 
+--   b.title,
+--   b.view_count,
+--   b.created_date,
+--   GROUP_CONCAT(distinct bf.uuid_file_name) uuid_file_name,
+--   h.head_content,
+--   m.name,
+--   (select 
+--     COUNT(*) 
+--   from 
+--     board_like bl 
+--   where 
+--     bl.board_no = b.board_no) like_count,
+--   GROUP_CONCAT(distinct c.content) comment_content,
+--   GROUP_CONCAT(distinct c.created_date) comment_date,
+--   GROUP_CONCAT(distinct r.content) reply_comment,
+--   GROUP_CONCAT(distinct r.created_date) reply_date,
+--   GROUP_CONCAT(distinct mc.name) comment_writer,
+--   GROUP_CONCAT(distinct mr.name) reply_writer
+-- from 
+--   board b 
+--   inner join head h on b.head_no = h.head_no
+--   inner join member m on b.member_no = m.member_no
+--   LEFT JOIN board_file bf on b.board_no = bf.board_no
+--   LEFT JOIN comment c on b.board_no = c.board_no
+--   LEFT JOIN reply r on c.comment_no = r.comment_no
+--   LEFT JOIN member mc on c.member_no = mc.member_no
+--   LEFT JOIN member mr on r.member_no = mr.member_no
+-- where
+--   b.board_no = 1;
 
 
 
@@ -263,3 +257,38 @@ delete from comment where comment_no="댓글번호";
 --답글 삭제
 delete from reply where reply_no="답글번호";
 
+
+
+
+
+select 
+  b.title,
+  b.view_count,
+  b.created_date,
+  GROUP_CONCAT(distinct bf.uuid_file_name) uuid_file_name,
+  h.head_content,
+  m.name,
+  (select 
+    COUNT(*) 
+  from 
+    board_like bl 
+  where 
+    bl.board_no = b.board_no) like_count,
+  c.content comment_content,
+  c.created_date comment_date,
+  r.content reply_comment,
+  r.created_date reply_date,
+  mc.name comment_writer,
+  mr.name reply_writer
+from 
+  board b 
+  inner join head h on b.head_no = h.head_no
+  inner join member m on b.member_no = m.member_no
+  LEFT JOIN board_file bf on b.board_no = bf.board_no
+  LEFT JOIN comment c on b.board_no = c.board_no
+  LEFT JOIN reply r on c.comment_no = r.comment_no
+  LEFT JOIN member mc on c.member_no = mc.member_no
+  LEFT JOIN member mr on r.member_no = mr.member_no
+where
+  b.board_no = 1
+group by 
