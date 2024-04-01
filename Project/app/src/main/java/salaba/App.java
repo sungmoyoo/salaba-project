4381de01e3ca054e59dc565638adc883d78b9d3c
kwanmo7 <kwanmo7@naver.com>
2024-04-01 15:55:21 +0900

    .

--- Project/app/src/main/java/salaba/App.java
+++ Project/app/src/main/java/salaba/App.java
@@ -3,12 +3,14 @@
  */
 package salaba;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@SpringBootApplication
+@EnableTransactionManagement
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class,args);
     }
 }
