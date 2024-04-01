4c4d28c7d3bfbaac162397f32ef08a33fd2d7729
kwanmo7 <kwanmo7@naver.com>
2024-04-01 18:00:54 +0900

    .

---
+++ Project/app/src/main/java/salaba/service/StorageService.java
@@ -0,0 +1,9 @@
+package salaba.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+  String upload(String bucketName, String path, MultipartFile multipartFile) throws Exception;
+
+  void delete(String bucketName, String path, String objectName)throws Exception;
+}
