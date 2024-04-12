ec5f381085fc7be6c91345a11d9923fe80148ce1
kwanmo7 <kwanmo7@naver.com>
2024-04-12 16:42:06 +0900

    .

--- Project/app/src/main/java/salaba/service/StorageService.java
+++ Project/app/src/main/java/salaba/service/StorageService.java
@@ -3,10 +3,7 @@ package salaba.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface StorageService {
+  String upload(String bucketName, String path, MultipartFile multipartFile) throws Exception;
 
-  String upload(String bucketName,
-      String path,
-      MultipartFile multipartFile) throws Exception;
-
-  void delete(String bucketName, String path, String objectName) throws Exception;
+  void delete(String bucketName, String path, String objectName)throws Exception;
 }
---
+++ Project/app/src/main/java/salaba/vo/rental_home/RentalHomeFacility.java
@@ -0,0 +1,23 @@
+package salaba.vo.rental_home;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class RentalHomeFacility implements Serializable { // 숙소 편의시설
+  private static final long serialVersionUID = 100L;
+
+  private int facilityNo; // 숙소 시설번호
+
+  @EqualsAndHashCode.Exclude
+  private String facilityName; // 숙소 시설명
+  
+  private int facilityCount; // 시설 갯수
+}
