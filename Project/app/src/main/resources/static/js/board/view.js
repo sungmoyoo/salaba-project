e5bb375dcbae6d7f25dba55fd99be42ff6463631
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-05-03 18:01:34 +0900

    .

--- Project/app/src/main/resources/static/js/board/view.js
+++ Project/app/src/main/resources/static/js/board/view.js
@@ -1,27 +1,29 @@
 "use strict";
 
-let replyForm = $('#reply-form');
-let reportForm = $('#report-form')
+let replyForm = $("#reply-form");
+let reportForm = $("#report-form");
 let boardNo = $("#boardNum").text(); // 게시글 번호
-// 댓글 작성
+
+//댓글 작성
 $("#addCommentBtn").click(function (e) {
   e.preventDefault(); // 기본 이벤트 방지
   let input = $("#commentContent"); // 댓글 내용
   let content = input.val(); // 입력 필드에 값 가져오기
-  if (content != "") { // 댓글 내용이 비어있지 않은 경우
+  if (content != "") {
+    // 댓글 내용이 비어있지 않은 경우
     $.ajax({
-      url: "/board/comment/add",  // 요청할 서버의 URL
-      type: "POST",        // HTTP 요청 방식
-      dataType: "json",    // 서버에서 반환되는 데이터 타입
-      data: {              // 서버로 보낼 데이터
+      url: "/board/comment/add", // 요청할 서버의 URL
+      type: "POST", // HTTP 요청 방식
+      dataType: "json", // 서버에서 반환되는 데이터 타입
+      data: {
+        // 서버로 보낼 데이터
         boardNo: boardNo,
         content: content,
       },
       success: function (data) {
         let newComment = $(`<div>
-                                    <div class="comment">
+                                    <div class="comment" data-no="${data.commentNo}">
                                         <div class="comment-text">
-                                          <span class="commentNo" hidden>${data.commentNo}</span>
                                           <span >${data.writer.nickname}</span>
                                           <span class="commentContent" data-th-text="*">${data.content}</span>
                                           <span data-th-text="*">${data.createdDate}</span>
@@ -38,11 +40,13 @@ $("#addCommentBtn").click(function (e) {
         newComment.children().find(".modi").click(modifyComment);
         // $('#box').find('.repo')
       },
-      error: function () { // 오류 메시지 날리기
-        alert("권한이 없습니다.");
+      error: function () {
+        // 오류 메시지 날리기
+        alert("권한이 없습니다.zsA");
       },
     });
-  } else { // 내용이 비어있는 경우
+  } else {
+    // 내용이 비어있는 경우
     alert("내용을 작성하세요");
   }
 });
@@ -54,10 +58,81 @@ $(".del").click(deleteComment);
 $(".modi").click(modifyComment);
 
 //댓글 - 답글달기
-$('#comment-box').on('click', '.comment-text', addReply);
+$("#comment-box").on("click", ".comment-text", addReplyForm);
 
-$('#addReplyBtn').click(event => {
-  replyForm.addClass('form-hidden');
+$("#addReplyBtn").click((event) => {
+  let commentNo = replyForm.parent().attr("data-no");
+  let replyContent = $("#replyContent").val();
+  console.log(commentNo, replyContent);
+  //   // AJAX 요청
+
+  //빈댓글 작성 금지
+  if (replyContent != "") {
+    $.ajax({
+      url: "/board/reply/add",
+      type: "POST",
+      dataType: "json",
+      data: {
+        commentNo: commentNo,
+        content: replyContent,
+      },
+      success: function (data) {
+        //기존의 답글달기 폼을 안보여준다.
+        replyForm.addClass("form-hidden");
+
+        let newReply = $(`<div class="reply">
+                                            <span class="replyNo" hidden>${data.replyNo}</span>
+                                            <span>${data.writer.nickname}</span>
+                                            <span class="replContent">${data.content}</span>
+                                            <span>${data.createdDate}</span>
+                                            <button class="del2">삭제</button>
+                                            <button class="modi2">수정</button>
+                                            <button class="report-btn reply-report-btn">신고</button>
+                                        </div>`);
+
+
+
+<div data-th-each="reply : ${comment.replyList}" data-th-object="${reply}">
+          <div class="reply">
+            └ <div class="textContent comment-text">
+              <span class="targetNo replyNo" data-th-text="*{replyNo}" hidden></span>
+              <!--프로필사진-->
+              <!-- <div>
+                <a data-th-href="@{'https://kr.object.ncloudstorage.com/tp3-salaba/member/' + ${member.photo}}" data-th-if="${member.photo}">
+                  <img data-th-src="@{'https://5ns6sjke2756.edge.naverncp.com/nBMc0TCJiv/member/' + ${member.photo}(type=f,w=80,h=80,ttype=jpg)}"></a><br>
+                <a data-th-unless="${member.photo}" href='/img/default-photo.jpeg'>
+                  <img height='80px' src='/img/default-photo.jpeg'></a><br>
+                <input name='file' type='file'>
+              </div>  -->
+              <span data-th-text="*{writer.nickname}">댓글 작성자</span>
+              <span class="replContent" data-th-text="*{content}">댓글 내용</span>
+              <span data-th-text="*{createdDate}">댓글 작성시간</span>
+            </div>
+              <button class="del2">삭제</button>
+              <button class="modi2">수정</button>
+              <button type="button" class="report-btn reply-report-btn">신고</button>
+          </div>
+        </div>
+
+
+
+
+
+
+        .parent().append(newReply);
+        newReply.find(".del2").click(deleteReply);
+        newReply.find(".modi2").click(modifyReply);
+      },
+      error: function () {
+        window.alert("권한이 없습니다.");
+      },
+    });
+  } else {
+    alert("내용을 작성하세요");
+  }
+
+  replyForm.addClass("form-hidden");
+  $("#replyContent").val(""); // 내용 지우기
 });
 
 //대댓글 삭제
@@ -69,7 +144,7 @@ $(".modi2").click(modifyReply);
 function deleteComment(e) {
   console.log($(this));
   let commentDiv = $(this).parent();
-  let commentNo = commentDiv.children().find('.commentNo').text();
+  let commentNo = commentDiv.children().find(".commentNo").text();
   console.log(commentNo);
   $.ajax({
     url: "/board/comment/delete",
@@ -144,75 +219,11 @@ function modifyComment(e) {
   }
 }
 
-/*답글 달기*/ 
-function addReply(e) {
-  console.log('==============>');
+/*답글 달기*/
+function addReplyForm(e) {
+  console.log("==============>");
   $(e.currentTarget).parent().append(replyForm);
-  replyForm.removeClass('form-hidden');  
-  
-  //e.stopPropagation();
-  // console.log("이벤트 전파 중지 완료");
-
-  // let replyForm = $(".replyForm");// 댓글에 대한 답글 폼 가져오기
-  // console.log("현재 페이지의 답글 폼:", replyForm.length > 0 ? "Y" : "N");
-
-  // let commentNo = $(this).find(".commentNo").text(); // 클릭한 댓글 번호
-  // console.log(commentNo);
-
-  if (
-    replyForm.length != 0 &&
-    replyForm.children().first().val() != commentNo
-  ) {
-    replyForm.remove();
-  }
-
-  const replyFormHTML = $(`<form class="replyForm">
-                                <input hidden value="${commentNo}">
-                                <textarea id="content" name="content"></textarea>
-                                <button type="button" id="addReplyBtn">답글 달기</button>
-                            </form>`);
-  replyFormHTML.find("#content").click((e) => e.stopPropagation());
-  let here = $(this);
-  if ($(".replyForm").length == 0 && here.find(".commentContent").length != 0) {
-    here.append(replyFormHTML);
-    here.find("#addReplyBtn").click(function (e) {
-      let newContent = here.find("#content").val();
-      //빈댓글 작성 금지
-      if (newContent != "") {
-        $.ajax({
-          url: "/board/reply/add",
-          type: "POST",
-          dataType: "json",
-          data: {
-            commentNo: commentNo,
-            content: newContent,
-          },
-          success: function (data) {
-            here.find(".replyForm").remove();
-            let newReply = $(`<div class="reply">
-                                            <span class="replyNo" hidden>${data.replyNo}</span>
-                                            <span>${data.writer.nickname}</span>
-                                            <span class="replContent">${data.content}</span>
-                                            <span>${data.createdDate}</span>
-                                            <button class="del2">삭제</button>
-                                            <button class="modi2">수정</button>
-                                            <button class="report-btn reply-report-btn">신고</button>
-                                        </div>`);
-            here.parent().append(newReply);
-            newReply.find(".del2").click(deleteReply);
-            newReply.find(".modi2").click(modifyReply);
-          },
-          error: function () {
-            window.alert("권한이 없습니다.");
-          },
-        });
-      } else {
-        alert("내용을 작성하세요");
-      }
-    });
-  } else {
-    here.find(".replyForm").remove();
-  }
+  replyForm.removeClass("form-hidden");
 }
 
 /* 답글 삭제 */
@@ -292,7 +303,7 @@ function modifyReply(e) {
   }
 }
 
-//추천수 
+//추천수
 let myLikeCount = parseInt($("#myLikeCount").text()); // 내 추천수(0 or 1)
 let initialCount = myLikeCount;
 console.log("myLikecount: ", myLikeCount);
@@ -315,74 +326,69 @@ likeButton.click(function () {
 });
 
 // beforeunload 이벤트 핸들러 추가
-  
-  $(window).on("beforeunload", function () {
-    let url = myLikeCount === 1 ? "/board/like" : "/board/unlike";
-    let boardNo = $("#boardNo").val();
-    if (initialCount != myLikeCount) {
-        $.ajax({
-        type: "POST",
-        url: url,
-        data: { boardNo: boardNo },
-        });
-    }
-  });
 
+$(window).on("beforeunload", function () {
+  let url = myLikeCount === 1 ? "/board/like" : "/board/unlike";
+  let boardNo = $("#boardNo").val();
+  if (initialCount != myLikeCount) {
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: { boardNo: boardNo },
+    });
+  }
+});
 
 // 신고창: 모달 사용
-$(document.body).on('click', '.report-btn', function (event) {
-   console.log(event.target);
-  let div = $(this).parent()
-  let targetNo = div.find('.textContent').find('.targetNo').text();
+$(document.body).on("click", ".report-btn", function (event) {
+  console.log(event.target);
+  let div = $(this).parent();
+  let targetNo = div.find(".textContent").find(".targetNo").text();
 
   let targetType;
 
-  if (div.attr('class') == 'comment') {
+  if (div.attr("class") == "comment") {
     targetType = 1;
-  } else if (div.attr('class') == 'reply'){
+  } else if (div.attr("class") == "reply") {
     targetType = 2;
   } else {
     targetNo = boardNo;
     targetType = 0;
   }
-  console.log("Target No:", targetNo);  
-  console.log("Target Type:", targetType);  
-
-
+  console.log("Target No:", targetNo);
+  console.log("Target Type:", targetType);
 
   // 모달 내 신고 대상 번호와 타입을 업데이트
   $("#reportModal").find("input[name='targetNo']").val(targetNo);
   $("#reportModal").find("input[name='targetType']").val(targetType);
 
   // 모달 열기
-  $("#reportModal").modal('show');
-
+  $("#reportModal").modal("show");
 });
 
+// 신고 제출 버튼을 클릭했을 때 폼을 AJAX로 전송
+$("#submitBtn").click(function (e) {
+  e.preventDefault(); // 기본 폼 제출을 막음
 
-  // 신고 제출 버튼을 클릭했을 때 폼을 AJAX로 전송
-  $("#submitBtn").click(function(e) {
-      e.preventDefault(); // 기본 폼 제출을 막음
+  // 폼 데이터 수집
+  let formData = new FormData($("#reportForm")[0]);
 
-      // 폼 데이터 수집
-      let formData = new FormData($("#reportForm")[0]);
-
-      $.ajax({
-          type: "POST",
-          url: "/board/report/add", 
-          data: formData,
-          processData: false,
-          contentType: false,
-          success: function (response) {
-              // 성공 콜백 함수
-              console.log("신고가 성공적으로 제출되었습니다.");
-              // 모달 닫기
-              $("#reportModal").modal("hide");
-              // 필요한 추가 로직
-          },
-          error: function(xhr, status, error) {
-              // 에러 콜백 함수
-              console.log("에러가 발생했습니다: ", error);
-          }
-      });
+  $.ajax({
+    type: "POST",
+    url: "/board/report/add",
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (response) {
+      // 성공 콜백 함수
+      console.log("신고가 성공적으로 제출되었습니다.");
+      // 모달 닫기
+      $("#reportModal").modal("hide");
+      // 필요한 추가 로직
+    },
+    error: function (xhr, status, error) {
+      // 에러 콜백 함수
+      console.log("에러가 발생했습니다: ", error);
+    },
   });
+});
